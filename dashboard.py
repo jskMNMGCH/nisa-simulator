205e4c5130cb09356cd162adf6194f0de0b6e807
jskMNMGCH <mnmgchjsk92@nt.phys.s.u-tokyo.ac.jp>
2025-04-30 11:35:31 +0900

    title revised

--- dashboard.py
+++ dashboard.py
@@ -59,8 +59,8 @@ def simulate_montecarlo(fund_settings, correlation_matrix, n_simulation=20000, y
     return np.array(total_history)
 
 def main():
-    st.set_page_config(page_title="NISA シミュレーション", layout="wide")
-    st.title("NISA シミュレーション")
+    st.set_page_config(page_title="つみたてシミュレーション", layout="wide")
+    st.title("つみたてシミュレーション")
 
     st.sidebar.header("シミュレーション設定")
     n_simulation = st.sidebar.slider("シミュレーション回数", 20000, 60000, 20000, step=5000)

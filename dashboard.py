457482566f4559c45b5d4a5dab08b2b18984f825
jskMNMGCH <mnmgchjsk92@nt.phys.s.u-tokyo.ac.jp>
2025-04-30 14:38:12 +0900

    color

--- dashboard.py
+++ dashboard.py
@@ -137,7 +137,7 @@ def main():
             )
             st.plotly_chart(fig, use_container_width=True)
 
-                with tab3:
+        with tab3:
             st.subheader(f"{n_years}年後 損益分布")
             profit = df["最終資産"] - principal
             lower = np.percentile(profit, 0)
